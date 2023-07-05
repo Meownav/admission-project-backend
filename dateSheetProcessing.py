dde0c11ff1f32958cbe0a52e5cf492f23edb0e81
arorAnjali <128891086+arorAnjali@users.noreply.github.com>
2023-07-05 23:14:25 +0530

    some updates

--- dateSheetProcessing.py
+++ dateSheetProcessing.py
@@ -2,8 +2,9 @@ import re
 import pandas as pd
 import pdfplumber
 import numpy as np
+import roman
 
-df = pd.DataFrame(columns=['TIME', 'DATE','UPC'])#data frame to store time,upc,date 
+df = pd.DataFrame(columns=['TIME', 'DATE','UPC','TERM']) #data frame to store time,upc,date 
 path="B.SC.(H) 2023-SEM.-II-IV-VI(CBCS)-LOCF-10-04-2023 (1).pdf"
 def processss_file(path):
     months = [
@@ -33,6 +34,7 @@ def processss_file(path):
     "December",
 ]
     days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+    semesters=["I", "II", "III", "IV", "V", "VI", "VII", "VIII"]
     pdf= pdfplumber.open(path)
     pages = pdf.pages
     print("total pages",len(pages))
@@ -56,27 +58,26 @@ def processss_file(path):
                 line_split=list(line.split())
                 for c in line_split:
                     if re.match(r'\d{7}',c):
-                        row=[d,y,c]
+                        row=[d,y,c,semester]
                         df.loc[len(df)]=row
+                        break
+                    elif any(sem==c for sem in semesters):
+                         semester =str(roman.fromRoman(c))+" SEMESTER"
+    return df
 def read_df():
     print(df.head())
 #     df.to_csv('datesheet_dataframe.csv',index=False)  #to download the csv file of the datesheet data frame(in case you want)
-def map_files():   #yhi hai vo function jo kaam nhi kr rha sad:(
-    data=pd.read_excel('student_data.xlsx')#data frame to store the students data excel file
-#     data['TIME OF EXAM']=np.nan  #insert empty column time of exam in data frame 'data'
-#     data['DATE OF EXAM']=np.nan
-    for a in range(len(data)):
-        for b in range(len(df)):
-            upc=df['UPC'][b]
-            t=df['TIME'][b]
-            data.loc[data["PAPER CODE"]==upc,"TIME OF EXAM"]=t
-#             if data['PAPER CODE'][a]==df['UPC'][b]:
-#                  data['TIME OF EXAM'][a]=df['TIME'][b]
-#                  data['DATE OF EXAM'][a]=df['DATE'][b]
-    print(data.head())
-#     data.to_csv('final_studentrecord.csv',index=False)
+def map_files(dateTimeDf):
+    studentDf =pd.read_excel('student_data.xlsx') #data frame to store the students data excel file
+    studentDf ["PAPER CODE"] = studentDf ["PAPER CODE"].astype(str)
+    dateTimeDf["UPC"] = dateTimeDf["UPC"].astype(str)
+    print(len(studentDf ))
+    mergedDf = pd.merge(
+        dateTimeDf, studentDf , left_on=["UPC","TERM"], right_on=["PAPER CODE","TERM"], how="right"
+    )
+
+    mergedDf.to_excel("final_studentrecordrecord.xlsx")
 
     
-processss_file(path)
-read_df() 
-map_files()
+dateTimeDf=processss_file(path)
+map_files(dateTimeDf)
