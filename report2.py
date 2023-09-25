806b9bc293d69a96d212de0a21fc79ad5d2abe8c
Utsav Sharma <sharmautsav2003@gmail.com>
2023-09-25 22:30:08 +0530

    better 2 genders

--- report2.py
+++ report2.py
@@ -1,16 +1,13 @@
 import pandas as pd
 df = pd.read_excel("./Data/ExternalData/dummy_uts.xlsx")
-x = df.groupby('Programme Name')
+df['Gender'].replace('M','Male',inplace=True)
+df['Gender'].replace('F','Female',inplace=True)
+x = df.groupby(['Programme Name','Gender'])
 out = x['Gender'].value_counts()
 out = pd.DataFrame(out)
 cols = list(set(df['Gender']))
 ind = list(set(df['Programme Name']))
 data = pd.DataFrame(data = out,columns=cols,index=ind)
-data['Male']+=data['M']
-data['Female']+=data['F']
-data.drop(['M','F'],axis='columns',inplace=True)
 for idx, row in out.iterrows():
-    if idx[1] in ['Male','Female']:
-        data.loc[idx] = row['count'] if row['count'] else "NAN"
+    data.loc[idx] = row['count'] if row['count'] else "NAN"
 data.to_excel("report2.xlsx")
-print(data)
