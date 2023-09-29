a5bac4448b0377a4ce66551da9b6e1e290f7f6f9
Utsav Sharma <sharmautsav2003@gmail.com>
2023-09-29 19:05:42 +0530

    with TOTAL in reports

--- report1.py
+++ report1.py
@@ -8,4 +8,8 @@ ind = list(set(df['Programme Name']))
 data = pd.DataFrame(data = out,columns=cols,index=ind)
 for idx, row in out.iterrows():
     data.loc[idx] = row['count'] if row['count'] else "NAN"
+
+for i in cols:
+    data.loc['Total',[i]] = data.loc[:,[i]].sum()
+print(data)
 data.to_excel("report1.xlsx")
--- report2.py
+++ report2.py
@@ -7,7 +7,11 @@ out = x['Gender'].value_counts()
 out = pd.DataFrame(out)
 cols = list(set(df['Gender']))
 ind = list(set(df['Programme Name']))
-data = pd.DataFrame(data = out,columns=cols,index=ind)
+data = pd.DataFrame(data = out,columns = cols,index = ind)
 for idx, row in out.iterrows():
     data.loc[idx] = row['count'] if row['count'] else "NAN"
+
+for i in cols:
+    data.loc['Total',[i]] = data.loc[:,[i]].sum()
+print(data)
 data.to_excel("report2.xlsx")
